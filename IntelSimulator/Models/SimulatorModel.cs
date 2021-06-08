7584b9334e092fb516c87be570b2f5c94822d51c
dumiensky <kacper@dumiensky.pl>
2021-06-08 23:45:14 +0200

    Abstracted Basic Registers logic to its' model

--- IntelSimulator/Models/SimulatorModel.cs
+++ IntelSimulator/Models/SimulatorModel.cs
@@ -3,19 +3,19 @@ using System.ComponentModel.DataAnnotations;
 
 namespace IntelSimulator.Models
 {
-    public class SimulatorModel
+    public class BasicRegisters
     {
-        public int AX { get; set; } = 10;
-        public int BX { get; set; } = 11;
+        public int AX { get; set; }
+        public int BX { get; set; }
         public int CX { get; set; }
         public int DX { get; set; }
 
-        public string AX_HEX => Convert.ToString(AX, 16);
-        public string BX_HEX => Convert.ToString(BX, 16);
-        public string CX_HEX => Convert.ToString(CX, 16);
-        public string DX_HEX => Convert.ToString(DX, 16);
+        public string AX_HEX => Convert.ToString(AX, 16).PadLeft(4, '0');
+        public string BX_HEX => Convert.ToString(BX, 16).PadLeft(4, '0');
+        public string CX_HEX => Convert.ToString(CX, 16).PadLeft(4, '0');
+        public string DX_HEX => Convert.ToString(DX, 16).PadLeft(4, '0');
 
-        public Inputs Inputs { get; set; } = new Inputs();
+        public FormInputs Inputs { get; set; } = new FormInputs();
 
         public void Reset()
         {
@@ -30,28 +30,48 @@ namespace IntelSimulator.Models
             CX = r.Next(0, 65536);
             DX = r.Next(0, 65536);
         }
-    }
 
-    public class Inputs
-    {
-        [RegularExpression("[0-9a-fA-F]{4}")]
-        public string AX { get; set; }
-        [RegularExpression("[0-9a-fA-F]{4}")]
-        public string BX { get; set; }
-        [RegularExpression("[0-9a-fA-F]{4}")]
-        public string CX { get; set; }
-        [RegularExpression("[0-9a-fA-F]{4}")]
-        public string DX { get; set; }
-
-        public void Clear()
+        public void ApplyInputs()
         {
-            AX = BX = CX = DX = string.Empty;
+            AX = ParseIfNotEmpty(Inputs.AX) ?? AX;
+            BX = ParseIfNotEmpty(Inputs.BX) ?? BX;
+            CX = ParseIfNotEmpty(Inputs.CX) ?? CX;
+            DX = ParseIfNotEmpty(Inputs.DX) ?? DX;
+
+            Inputs.Clear();
         }
 
-        public bool Any() =>
-            !string.IsNullOrWhiteSpace(AX) ||
-            !string.IsNullOrWhiteSpace(BX) ||
-            !string.IsNullOrWhiteSpace(CX) ||
-            !string.IsNullOrWhiteSpace(DX);
+        private int? ParseIfNotEmpty(string input)
+        {
+            if (!string.IsNullOrWhiteSpace(input))
+            {
+                return int.Parse(input, System.Globalization.NumberStyles.HexNumber);
+            }
+
+            return null;
+        }
+
+        public class FormInputs
+        {
+            [RegularExpression("[0-9a-fA-F]{4}")]
+            public string AX { get; set; }
+            [RegularExpression("[0-9a-fA-F]{4}")]
+            public string BX { get; set; }
+            [RegularExpression("[0-9a-fA-F]{4}")]
+            public string CX { get; set; }
+            [RegularExpression("[0-9a-fA-F]{4}")]
+            public string DX { get; set; }
+
+            public void Clear()
+            {
+                AX = BX = CX = DX = string.Empty;
+            }
+
+            public bool Any() =>
+                !string.IsNullOrWhiteSpace(AX) ||
+                !string.IsNullOrWhiteSpace(BX) ||
+                !string.IsNullOrWhiteSpace(CX) ||
+                !string.IsNullOrWhiteSpace(DX);
+        }
     }
 }
