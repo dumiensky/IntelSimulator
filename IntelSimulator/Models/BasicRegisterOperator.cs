6e982614ae22452bb72747a706adb7d199bbc932
dumiensky <kacper@dumiensky.pl>
2021-06-09 00:14:59 +0200

    MOV & XCHG implementation

---
+++ IntelSimulator/Models/BasicRegisterOperator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+namespace IntelSimulator.Models
+{
+    public class BasicRegisterOperator
+    {
+        readonly BasicRegisters _basicRegisters;
+        readonly RegisterSelector _registerSelector;
+
+        public BasicRegisterOperator(BasicRegisters basicRegisters, RegisterSelector registerSelector)
+        {
+            _basicRegisters = basicRegisters;
+            _registerSelector = registerSelector;
+        }
+
+        public void MOV()
+        {
+            SetRegister(_registerSelector.Destination, GetRegisterValue(_registerSelector.Source));
+        }
+
+        public void XCHG()
+        {
+            var val1 = GetRegisterValue(_registerSelector.Source);
+            var val2 = GetRegisterValue(_registerSelector.Destination);
+
+            SetRegister(_registerSelector.Source, val2);
+            SetRegister(_registerSelector.Destination, val1);
+        }
+
+        private void SetRegister(Register which, int value)
+        {
+            switch (which)
+            {
+                case Register.AX:
+                    _basicRegisters.AX = value;
+                    break;
+                case Register.BX:
+                    _basicRegisters.BX = value;
+                    break;
+                case Register.CX:
+                    _basicRegisters.CX = value;
+                    break;
+                case Register.DX:
+                    _basicRegisters.DX = value;
+                    break;
+            }
+        }
+
+        private int GetRegisterValue(Register which)
+        {
+            switch (which)
+            {
+                case Register.AX:
+                    return _basicRegisters.AX;
+                case Register.BX:
+                    return _basicRegisters.BX;
+                case Register.CX:
+                    return _basicRegisters.CX;
+                case Register.DX:
+                    return _basicRegisters.DX;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
---
+++ IntelSimulator/Models/RegisterSelector.cs
@@ -0,0 +1,58 @@
+﻿using System;
+namespace IntelSimulator.Models
+{
+    public class RegisterSelector
+    {
+        private Register source = Register.AX;
+        private Register destination = Register.BX;
+
+        public Register Source
+        {
+            get => source;
+            set
+            {
+                source = value;
+                CheckForConflict(nameof(source));
+            }
+        }
+
+        public Register Destination
+        {
+            get => destination;
+            set
+            {
+                destination = value;
+                CheckForConflict(nameof(destination));
+            }
+        }
+
+        private void CheckForConflict(string caller)
+        {
+            if(source == destination)
+            {
+                if(caller == nameof(source))
+                {
+                    if (destination == Register.DX)
+                        destination = Register.AX;
+                    else
+                        destination++;
+                }
+                else
+                {
+                    if (source == Register.DX)
+                        source = Register.AX;
+                    else
+                        source++;
+                }
+            }
+        }
+    }
+
+    public enum Register
+    {
+        AX,
+        BX,
+        CX,
+        DX
+    }
+}
